926f4c0b1d618473e2a558fd5e9e72eec6182874
Igor C. Brasil <igorbrasilc@hotmail.com>
2022-05-12 17:02:34 -0300

    adds confirmPassword validation and inserts into DB the products

--- src/schemas/authSchemas.js
+++ src/schemas/authSchemas.js
@@ -3,7 +3,8 @@ import joi from 'joi';
 const signUpSchema = joi.object({
     name: joi.string().required(),
     email: joi.string().email().required(),
-    password: joi.string().required()
+    password: joi.string().required(),
+    confirmPassword: joi.ref('password')
 });
 
 const signInSchema = joi.object({
